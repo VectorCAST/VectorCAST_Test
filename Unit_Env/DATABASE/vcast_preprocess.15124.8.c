1358a84611074b45e399434fb035666cacdd892a
KimSangA1118 <sanga@hancommds.com>
2018-07-20 16:49:46 +0900

    'Original_up'

--- Unit_Env/DATABASE/vcast_preprocess.15124.8.c
+++
@@ -1,9 +0,0 @@
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_PREFIX_END;
-#include "C:/VCAST/Tutorial/c/database.c"
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_START;
-
-typedef int VECTORCAST_MARKER__UNIT_APPENDIX_END;
